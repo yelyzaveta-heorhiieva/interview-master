de09cdab4bce9dabddd656a5610916e2e12b7901
Yelyzaveta Heorhiieva <heorhiieva.y@gmail.com>
2025-10-01 20:45:19 +0300

    change git ignore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'interview-master';
+  messageFromParent = 'I am your father'
+  messageFromChild = '';
+
+  getMessageFromChild(message: string) {
+    this.messageFromChild = message
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AngularComponent } from './components/angular/angular.component';
+import { TypescriptComponent } from './components/typescript/typescript.component';
+import { JavascriptComponent } from './components/javascript/javascript.component';
+import { RxjsComponent } from './components/rxjs/rxjs.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'angular', component: AngularComponent },
+  { path: 'typescript', component: TypescriptComponent },
+  { path: 'javascript', component: JavascriptComponent },
+  { path: 'rxjs', component: RxjsComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
